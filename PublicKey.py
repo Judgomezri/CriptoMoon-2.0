2f703d6cd7fc1b973083d78453f933c34be2b2bf
judgomezri <judgomezri@unal.edu.co>
2022-04-20 21:10:07 -0500

    Implementación de Llave Pública Completa

--- PublicKey.py
+++ PublicKey.py
@@ -1,5 +1,7 @@
 from sympy import N, randprime
 import random
+import math
+
 #General functions
 def generatePrime(a=0):
     if a!=0:
@@ -115,48 +117,36 @@ def cifrarRSA(message, r, pub_key):
     message=message.upper()
     message=message.replace(" ", "")
     encrypt_text = []
-    block=[[1,False]]
-    while(len(message)%3!=0): #De ser necesario completamos el texto con "X" para que el tamaño sea múltiplo de 3 
-        message=message+"X"
-    for i in range(0,len(message),3):#CIFRAMOS EN BLOQUES DE 3 
-        if ord(message[i])-64==26: #verifica y cambia el valor de z para que al hacer la conversion de valores den letras del alfabeto ingles
-            block[0][0]=(ord(message[i])-65)*(26**2)+(ord(message[i+1])-65)*26+(ord(message[i+2])-65)
-            block[0][1]=True
-            cipher_text = [pow(block[0][0],pub_key,r),block[0][1]]
-            encrypt_text.append(cipher_text)
-        else:         
-            block[0][0]=(ord(message[i])-64)*(26**2)+(ord(message[i+1])-65)*26+(ord(message[i+2])-65)
-            block[0][1]=False
-            print(block[0][0])
-            cipher_text = [pow(block[0][0],pub_key,r),block[0][1]]
-            encrypt_text.append(cipher_text)
+    ca=[]
+    aux=len(message) #LEN ORIGINAL MENSAJE
+    while(len(message)%3!=0):
+      message=message+"X"
+    for i in range(0,len(message),3):#CIFRAMOS EN BLOQUES DE 3           
+          c=(ord(message[i])-64)*(27**2)+(ord(message[i+1])-64)*27+(ord(message[i+2])-64)
+          ca.append(c)
+          cipher_text = pow(c,pub_key,r)#(m**pub_key)%n
+          encrypt_text.append(cipher_text)
+    #print(ca)
     return(encrypt_text)
 
 def descifrarRSA(message, r):
-    file_in = open("private_key_RSA.txt", "r")
+    file_in = open("priv_key_RSA.txt", "r")
     priv_key = int(file_in.read())
     file_in.close()
     decrypt_text = ""
     convert10=0
+    decryptleter=[]
     for k in range(0,len(message)):
-        c = message[k][0]
-        decrypt_letter = (c**priv_key)%r
-        convert26 = fromDeci("", 26, decrypt_letter)
+        c = message[k]
+        decrypt_letter = pow(c,priv_key,r)
+        decryptleter.append(decrypt_letter)
+        convert27 = fromDeci("", 27, decrypt_letter)
 
-        for i in range(0,len(convert26)):
-             if message[k][1]==True : #Basicamente verifica si el bloque empieza por z.
-              convert10= toDeci(convert26[i],26)
-              convertascci=chr(convert10+65)
-              decrypt_text = decrypt_text + convertascci
-             else:
-               if(i==0):
-                convert10= toDeci(convert26[i],26)
+        for i in range(0,len(convert27)):
+                convert10= toDeci(convert27[i],27)
                 convertascci=chr(convert10+64)
                 decrypt_text = decrypt_text + convertascci
-               else:
-                convert10= toDeci(convert26[i],26)
-                convertascci=chr(convert10+65)
-                decrypt_text = decrypt_text + convertascci
+    #print(decryptleter,"d")
     return(decrypt_text)
 
 #Rabin -----------------------------------------------------------------
@@ -266,7 +256,7 @@ def generateElgamalData(p, alpha=0):
     return alpha, beta
     
 #For asymetric encryption
-def cifrargamal(message,p,beta,alpha):
+def cifrarElgamal(message,p,beta,alpha):
     message=message.upper()
     message=message.replace(" ", "")
     k=gen_key(p)
@@ -279,10 +269,22 @@ def cifrargamal(message,p,beta,alpha):
           c=(ord(message[i])-64)*(27**2)+(ord(message[i+1])-64)*27+(ord(message[i+2])-64)
           cipher_text = c*alpha_a_k
           encrypt_text.append(cipher_text)
-    return(encrypt_text,alpha_k)
+    file_out = open("ak.txt", "w")
+    file_out.write(str(alpha_k))
+    file_out.close()
+    return(encrypt_text)
 
 #For decryption
-def decifrargamal(message,alpha_k,a,p):
+def descifrarElgamal(message):
+    file_in = open("ak.txt", "r")
+    alpha_k = int(file_in.read())
+    file_in.close()
+    file_in = open("pub_key_elgamal.txt", "r")
+    p = int(file_in.readline().rstrip())
+    file_in.close()
+    file_in = open("priv_key_elgamal.txt", "r")
+    a = int(file_in.read())
+    file_in.close()
     decrypt_text = ""
     convert10=0
     for k in range(0,len(message)):
@@ -295,3 +297,271 @@ def decifrargamal(message,alpha_k,a,p):
               convertascci=chr(convert10+64)
               decrypt_text = decrypt_text + convertascci
     return(decrypt_text)
+
+#Menezes Vanestone--------------------------------------------------------
+
+def legendre(a, p):
+    return pow(a, (p - 1) // 2, p)
+
+def tonelli(n, p):
+    q = p - 1
+    s = 0
+    while q % 2 == 0:
+        q //= 2
+        s += 1
+    if s == 1:
+        return pow(n, (p + 1) // 4, p)
+    for z in range(2, p):
+        if p - 1 == legendre(z, p):
+            break
+    c = pow(z, q, p)
+    r = pow(n, (q + 1) // 2, p)
+    t = pow(n, q, p)
+    m = s
+    t2 = 0
+    while (t - 1) % p != 0:
+        t2 = (t * t) % p
+        for i in range(1, m):
+            if (t2 - 1) % p == 0:
+                break
+            t2 = (t2 * t2) % p
+        b = pow(c, 1 << (m - i - 1), p)
+        r = (r * b) % p
+        c = (b * b) % p
+        t = (t * c) % p
+        m = i
+    return r
+
+def ec_gen_points_set(a, b, p):
+    ec_points_on_curve = []
+    for x in range(p):
+        y2 = x ** 3 + a * x + b
+        lengdre_val = legendre(y2, p)
+        if lengdre_val != 0:
+            if lengdre_val != 1:  # (y2 | p) must be ≡ 1 to have a square if not continue to next num
+                continue
+        elif lengdre_val == 0:
+            y_root1 = 0
+            gen1 = (x, y_root1)
+            ec_points_on_curve.append(gen1)
+            continue
+        y_root1 = tonelli(y2, p)  # one possible root
+        y_root2 = p - y_root1
+
+        if y_root2 < y_root1:
+            gen1 = (x, y_root2)
+            gen2 = (x, y_root1)
+        else:
+            gen1 = (x, y_root1)
+            gen2 = (x, y_root2)
+
+        ec_points_on_curve.append(gen1)
+        ec_points_on_curve.append(gen2)
+    return ec_points_on_curve
+
+def extended_gcd(aa, bb):
+    lastremainder, remainder = abs(aa), abs(bb)
+    x, lastx, y, lasty = 0, 1, 1, 0
+    while remainder:
+        lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
+        x, lastx = lastx - quotient * x, x
+        y, lasty = lasty - quotient * y, y
+    return lastremainder, lastx * (-1 if aa < 0 else 1), lasty * (-1 if bb < 0 else 1)
+
+
+# calculate `modular inverse`
+def modinv(a, m):
+    g, x, y = extended_gcd(a, m)
+    if g == 1:
+        return x % m
+    else:
+        return None
+
+#OPERACIONES ECC
+# double function
+def ecc_double(x1, y1, p, a):
+    if modinv(2 * y1, p) is None: return None
+
+    s = ((3 * (x1 ** 2) + a) * modinv(2 * y1, p)) % p
+    x3 = (s ** 2 - x1 - x1) % p
+    y3 = (s * (x1 - x3) - y1) % p
+    return (x3, y3)
+
+
+# add function
+def ecc_add(x1, y1, x2, y2, p, a):
+    s = 0
+    if (x1 == x2):
+        if modinv(2 * y1, p) is None : return None
+        s = ((3 * (x1 ** 2) + a) * modinv(2 * y1, p)) % p
+    else:
+        if modinv(x2 - x1, p) is None: return None
+        s = ((y2 - y1) * modinv(x2 - x1, p)) % p
+    x3 = (s ** 2 - x1 - x2) % p
+    y3 = (s * (x1 - x3) - y1) % p
+    return (x3, y3)
+
+
+def double_and_add(multi, generator, p, a):
+    (x3, y3) = (0, 0)
+    (x1, y1) = generator
+    (x_tmp, y_tmp) = generator
+    init = 0
+    for i in str(bin(multi)[2:]):
+        if (i == '1') and (init == 0):
+            init = 1
+        elif (i == '1') and (init == 1):
+            if ecc_double(x_tmp, y_tmp, p, a) is None: return None
+            if ecc_add(x1, y1, x3, y3, p, a) is None: return None
+            (x3, y3) = ecc_double(x_tmp, y_tmp, p, a)
+            (x3, y3) = ecc_add(x1, y1, x3, y3, p, a)
+            (x_tmp, y_tmp) = (x3, y3)
+        else:
+            if ecc_double(x_tmp, y_tmp, p, a) is None: return None
+            (x3, y3) = ecc_double(x_tmp, y_tmp, p, a)
+            (x_tmp, y_tmp) = (x3, y3)
+    return (x3, y3)
+
+
+def scale_point_set(a, b, p, generator):
+    ec_curve_points = ec_gen_points_set(a, b, p)
+    scaled_points_set = {}
+    i = 1
+    scaled_points_set[str(i) + "P"] = generator
+
+    while True:
+        i += 1
+        scaled_point = double_and_add(i, generator, p, a)
+
+        if scaled_point is None or scaled_point not in ec_curve_points:
+            scaled_points_set[str(i) + "P"] = "O"
+            break
+
+        elif scaled_point in ec_curve_points:
+            scaled_points_set[str(i) + "P"] = scaled_point
+            if str(i) + "P" != "2P" and scaled_points_set["2P"] == scaled_points_set[str(i) + "P"]:  # the current index is not 2P and no other duplicate point exists
+                scaled_points_set[str(i) + "P"] = "O"
+                break    #if duplicate P exists then stop
+
+    return scaled_points_set
+
+def KeyGen(key, pointSet):
+    order = len(pointSet)
+    if key > order:
+        key = key % order
+
+        if key == 0: return pointSet[str(order) + "P"]  # if the modulo is zero return the last element in list
+
+        key = pointSet[str(key) + "P"]  # User Public Key
+    else:
+        key = pointSet[str(key) + "P"]  # User Public Key
+
+    return key
+
+# agoritmo extendido de euclides que toma 2 primos relativos y les encuentra inverso modular
+def Inverse_Mod(e, m):
+    m0 = m
+    y = 0
+    x = 1
+    if (m == 1):
+        return 0
+    while (e > 1):
+        q = int(e / m)  # q is quotient
+        temp = m
+        m = e % m
+        e = temp
+        temp = y
+        # Update x and y
+        y = x - q * y
+        x = temp
+    # Make x positive
+    if (x < 0): x = x + m0
+    return x
+def primitivo(a,b,p):
+	for x in range(1,p):
+		val=((x*x*x)+a*x+b) % p
+		res = math.sqrt(val)
+		if (abs(res-int(res))<0.0001):
+			return(x,int(res))
+
+def cifrarMenezesVanestone(text):
+    file_in = open("pub_key_MV.txt", "r")
+    a = int(file_in.readline().rstrip())
+    b = int(file_in.readline().rstrip())
+    p = int(file_in.readline().rstrip())
+    gx = int(file_in.readline().rstrip())
+    gy = int(file_in.readline().rstrip())
+    Nb = int(file_in.readline().rstrip())
+    file_in.close()
+    file_in = open("priv_key_MV.txt", "r")
+    Ka = int(file_in.read())
+    file_in.close()
+    cifrado=[]
+    generator=(gx,gy)
+    y0 = KeyGen(Ka, scale_point_set(a, b, p, generator))
+    publicKeyB = KeyGen(Nb, scale_point_set(a, b, p, generator))
+    mask = KeyGen(Ka, scale_point_set(a, b, p, publicKeyB))
+    for i in range(len(text)):
+        m=str(ord(text[i]))
+        if((len(m)%2)!=0):
+            m1 = int(m[:(len(m)//2)+1])
+            m2 = int(m[len(m)//2+1:])
+        else:
+            m1 = int(m[:(len(m)//2)])
+            m2 = int(m[len(m)//2:])
+        y1 = (mask[0]*m1)%p
+        y2 = (mask[1]*m2)%p
+        cifrado.append((y0,y1,y2))
+    #print("(C1,C2)):", mask)
+    return(cifrado)
+  
+def descifrarMenezesVanestone(tcifrado):
+    file_in = open("pub_key_MV.txt", "r")
+    a = int(file_in.readline().rstrip())
+    b = int(file_in.readline().rstrip())
+    p = int(file_in.readline().rstrip())
+    gx = int(file_in.readline().rstrip())
+    gy = int(file_in.readline().rstrip())
+    Nb = int(file_in.readline().rstrip())
+    file_in.close()
+    TextoDecifrado=""
+    for i in range(len(tcifrado)):
+        Nb_hint= KeyGen(Nb, scale_point_set(a, b, p, tcifrado[i][0]))
+        inv_c1 = Inverse_Mod(Nb_hint[0],p)
+        decrypt_m1 = (inv_c1*tcifrado[i][1])%p
+        inv_c2 = Inverse_Mod(Nb_hint[1],p)
+        decrypt_m2 = (inv_c2*tcifrado[i][2])%p
+        TextoDecifrado = TextoDecifrado + chr(int(str(decrypt_m1) + str(decrypt_m2)))
+    return TextoDecifrado
+
+def generateMvData(p):
+    if p=="":
+        p=randprime(100,1000)
+    else:
+        p=int(p)
+    a=random.randint(0,100)
+    b=random.randint(0,100)
+    gx,gy=primitivo(a,b,p)
+    ca=random.randint(10**2,10**3)
+    while(ca>p):
+        ca=random.randint(10**2,10**3)
+    cb=random.randint(10**2,10**3)
+    while(cb>p):
+        cb=random.randint(10**2,10**3)
+    file_out = open("pub_key_MV.txt", "w")
+    file_out.write(str(a))
+    file_out.write("\n")
+    file_out.write(str(b))
+    file_out.write("\n")
+    file_out.write(str(p))
+    file_out.write("\n")
+    file_out.write(str(gx))
+    file_out.write("\n")
+    file_out.write(str(gy))
+    file_out.write("\n")
+    file_out.write(str(cb))
+    file_out.close()
+    file_out = open("priv_key_MV.txt", "w")
+    file_out.write(str(ca))
+    file_out.close()
+    return a,b,p,gx, gy, ca, cb
--- main.py
+++ main.py
@@ -7,11 +7,32 @@ from sympy import randprime
 #Loading the interface properties
 Builder.load_file('CriptoMoon.kv')
 
+def list_to_str(list):
+    s=[]
+    for i in range(0,len(list)):
+        s.append(str(list[i]))
+    cstring="----".join(s)
+    return cstring
+
+def str_to_list(text):
+  lista=text.split("----") #esto transforma de nuevo el texto de numeros a lista
+  s=[]
+  for i in range(0,len(lista)):
+    s.append(int(lista[i]))
+  return s
+
 # Declare both screens
 class MenuScreen(Screen):
     pass
 class LlavePublica_Screen(Screen):
-    def spinner_clicked(self, cifrador, grid):
+    def spinner_clicked(self, cifrador, grid, primo1, primo2, data1, data2, data3, data4, data5):
+        primo1.select_all(); primo1.delete_selection()
+        primo2.select_all(); primo2.delete_selection()
+        data1.select_all(); data1.delete_selection()
+        data2.select_all(); data2.delete_selection()
+        data3.select_all(); data3.delete_selection()
+        data4.select_all(); data4.delete_selection()
+        data5.select_all(); data5.delete_selection()
         if cifrador == "RSA" or cifrador=="Rabin" or cifrador=="ElGamal" or cifrador=="Menezes Vanestone":
             grid.visible = True
         else:
@@ -23,7 +44,7 @@ class LlavePublica_Screen(Screen):
             primo2=str(pk.generatePrime())
             widget.select_all(); widget.delete_selection()    
             widget.insert_text(primo1)
-            widget2.select_all(); widget.delete_selection()    
+            widget2.select_all(); widget2.delete_selection()    
             widget2.insert_text(primo2)
         elif cifrador== "Rabin":
             primo1=pk.generatePrime()
@@ -40,15 +61,19 @@ class LlavePublica_Screen(Screen):
             primo1=str(pk.generatePrime())
             widget.select_all(); widget.delete_selection()    
             widget.insert_text(primo1)
+        elif cifrador== "Menezes Vanestone":
+            primo1=str(pk.generatePrime(10))
+            widget.select_all(); widget.delete_selection()    
+            widget.insert_text(primo1)
 
     def generarDatos(self, cifrador, primo1, primo2, data1, data2, data3, data4, data5):
         if cifrador== "RSA":
             n, phi, pub_key = pk.generateRsaData(int(primo1.text), int(primo2.text))
             data1.select_all(); data1.delete_selection()    
             data1.insert_text(str(n))
-            data2.select_all(); data1.delete_selection()    
+            data2.select_all(); data2.delete_selection()    
             data2.insert_text(str(phi))
-            data3.select_all(); data1.delete_selection()    
+            data3.select_all(); data3.delete_selection()    
             data3.insert_text(str(pub_key))
         elif cifrador== "Rabin":
             n = pk.generateRabinData(int(primo1.text),int(primo2.text))
@@ -58,25 +83,76 @@ class LlavePublica_Screen(Screen):
             alpha, beta = pk.generateElgamalData(int(primo1.text))
             data1.select_all(); data1.delete_selection()    
             data1.insert_text(str(alpha))
-            data2.select_all(); data1.delete_selection()    
+            data2.select_all(); data2.delete_selection()    
             data2.insert_text(str(beta))
+        elif cifrador== "Menezes Vanestone":
+            a,b,p,gx, gy, ca, cb = pk.generateMvData(int(primo1.text))
+            data1.select_all(); data1.delete_selection()    
+            data1.insert_text(str(a))
+            data2.select_all(); data2.delete_selection()    
+            data2.insert_text(str(b))
+            data4.select_all(); data4.delete_selection()    
+            data4.insert_text(str(gx))
+            data5.select_all(); data5.delete_selection()    
+            data5.insert_text(str(gy))
         else:
             pass
     def cifrar(self, cifrador, primo1, primo2, data1, data2, data3, data4, data5, Texto, Resultado):
         if cifrador== "RSA":
+            text=pk.cifrarRSA(Texto.text, int(data1.text), int(data3.text))
+            text=list_to_str(text)
             Resultado.select_all(); Resultado.delete_selection()   
-            Resultado.insert_text(str(pk.cifrarRSA(Texto.text, int(data1.text), int(data3.text))))
+            Resultado.insert_text(text)
         elif cifrador== "Rabin":
+            text=pk.cifrarRabin(Texto.text, int(data1.text))
+            text=list_to_str(text)
             Resultado.select_all(); Resultado.delete_selection()   
-            Resultado.insert_text(str(pk.cifrarRabin(Texto.text, int(data1.text))))
+            Resultado.insert_text(text)
+        elif cifrador== "ElGamal":
+            text=pk.cifrarElgamal(Texto.text, int(primo1.text), int(data2.text), int(data1.text))
+            text=list_to_str(text)
+            Resultado.select_all(); Resultado.delete_selection()   
+            Resultado.insert_text(text)
+        elif cifrador== "Menezes Vanestone":
+            text=pk.cifrarMenezesVanestone(Texto.text)
+            Resultado.select_all(); Resultado.delete_selection()   
+            Resultado.insert_text(str(pk.cifrarMenezesVanestone(Texto.text)))
 
     def descifrar(self, cifrador, primo1, primo2, data1, data2, data3, data4, data5, Texto, Resultado):
         if cifrador== "RSA":
+            text=str_to_list(Texto.text)
             Resultado.select_all(); Resultado.delete_selection()
-            Resultado.insert_text(str(pk.descifrarRSA(Texto.text, int(data1.text))))
+            Resultado.insert_text(pk.descifrarRSA(text, int(data1.text)))
         elif cifrador=="Rabin":
+            text=str_to_list(Texto.text)
+            text=pk.descifrarRabin(text)
+            if int(primo1.text)>99999:
+                posibleText = ""
+                for i in text:
+                    for j in i:
+                        if len(j)==3:
+                            posibleText=posibleText+j
+                text=posibleText
             Resultado.select_all(); Resultado.delete_selection()
-            Resultado.insert_text(str(pk.descifrarRabin(Texto.text, int(data1.text))))
+            Resultado.insert_text(str(text))
+        elif cifrador=="ElGamal":
+            text=str_to_list(Texto.text)
+            Resultado.select_all(); Resultado.delete_selection()
+            Resultado.insert_text(str(pk.descifrarElgamal(text)))
+        elif cifrador=="Menezes Vanestone":
+            text=list(Texto.text)
+            text[0]='['
+            text[-1]=']'
+            text="".join(text).replace(" ","").replace("},{","],[").replace("{","(").replace("}",")")
+            text="["+text+"]"
+            text = list(text)
+            text[1]=""
+            text[-2]=""
+            text="".join(text)
+            print(text)
+            text=eval(text)
+            Resultado.select_all(); Resultado.delete_selection()
+            Resultado.insert_text(str(pk.descifrarMenezesVanestone(text)))
 
 class CriptoMoon(App):
     def build(self):
