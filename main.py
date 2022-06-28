855c5c1f1b9b182ce841869e247db5ff45e23bd9
judgomezri <judgomezri@unal.edu.co>
2022-06-27 23:47:12 -0500

    Manejo de errores

--- main.py
+++ main.py
@@ -32,141 +32,159 @@ def str_to_list(text):
 # Declare both screens
 class MenuScreen(Screen):
     def Full_Blockchain(self):
-        text = ""
-        for i in Bc.Full_blockchain(Bc.MoonCoins):
-            text = text+i
-            Full_Blockchain_Screen.setText(text)
+        try:
+            text = ""
+            for i in Bc.Full_blockchain(Bc.MoonCoins):
+                text = text+i
+                Full_Blockchain_Screen.setText(text)
+        except:
+            pass
 
 
 class LlavePublica_Screen(Screen):
     def spinner_clicked(self, cifrador, grid, primo1, primo2, data1, data2, data3, data4, data5):
-        primo1.select_all(); primo1.delete_selection()
-        primo2.select_all(); primo2.delete_selection()
-        data1.select_all(); data1.delete_selection()
-        data2.select_all(); data2.delete_selection()
-        data3.select_all(); data3.delete_selection()
-        data4.select_all(); data4.delete_selection()
-        data5.select_all(); data5.delete_selection()
-        if cifrador == "RSA" or cifrador=="Rabin" or cifrador=="ElGamal" or cifrador=="Menezes Vanestone":
-            grid.visible = True
-        else:
-            grid.visible = False
+        try:
+            primo1.select_all(); primo1.delete_selection()
+            primo2.select_all(); primo2.delete_selection()
+            data1.select_all(); data1.delete_selection()
+            data2.select_all(); data2.delete_selection()
+            data3.select_all(); data3.delete_selection()
+            data4.select_all(); data4.delete_selection()
+            data5.select_all(); data5.delete_selection()
+            if cifrador == "RSA" or cifrador=="Rabin" or cifrador=="ElGamal" or cifrador=="Menezes Vanestone":
+                grid.visible = True
+            else:
+                grid.visible = False
+        except:
+            pass
 
     def generarPrimos(self, cifrador, widget, widget2):
-        if cifrador== "RSA":
-            primo1=str(pk.generatePrime())
-            primo2=str(pk.generatePrime())
-            widget.select_all(); widget.delete_selection()    
-            widget.insert_text(primo1)
-            widget2.select_all(); widget2.delete_selection()    
-            widget2.insert_text(primo2)
-        elif cifrador== "Rabin":
-            primo1=pk.generatePrime()
-            while(primo1%4!=3):
+        try:
+            if cifrador== "RSA":
+                primo1=str(pk.generatePrime())
+                primo2=str(pk.generatePrime())
+                widget.select_all(); widget.delete_selection()    
+                widget.insert_text(primo1)
+                widget2.select_all(); widget2.delete_selection()    
+                widget2.insert_text(primo2)
+            elif cifrador== "Rabin":
                 primo1=pk.generatePrime()
-            primo2=pk.generatePrime()
-            while(primo2%4!=3):
+                while(primo1%4!=3):
+                    primo1=pk.generatePrime()
                 primo2=pk.generatePrime()
-            widget.select_all(); widget.delete_selection()    
-            widget.insert_text(str(primo1))
-            widget2.select_all(); widget.delete_selection()    
-            widget2.insert_text(str(primo2))
-        elif cifrador== "ElGamal":
-            primo1=str(pk.generatePrime())
-            widget.select_all(); widget.delete_selection()    
-            widget.insert_text(primo1)
-        elif cifrador== "Menezes Vanestone":
-            primo1=str(pk.generatePrime(10))
-            widget.select_all(); widget.delete_selection()    
-            widget.insert_text(primo1)
+                while(primo2%4!=3):
+                    primo2=pk.generatePrime()
+                widget.select_all(); widget.delete_selection()    
+                widget.insert_text(str(primo1))
+                widget2.select_all(); widget.delete_selection()    
+                widget2.insert_text(str(primo2))
+            elif cifrador== "ElGamal":
+                primo1=str(pk.generatePrime())
+                widget.select_all(); widget.delete_selection()    
+                widget.insert_text(primo1)
+            elif cifrador== "Menezes Vanestone":
+                primo1=str(pk.generatePrime(10))
+                widget.select_all(); widget.delete_selection()    
+                widget.insert_text(primo1)
+        except:
+            pass
 
     def generarDatos(self, cifrador, primo1, primo2, data1, data2, data3, data4, data5):
-        if cifrador== "RSA":
-            n, phi, pub_key = pk.generateRsaData(int(primo1.text), int(primo2.text))
-            data1.select_all(); data1.delete_selection()    
-            data1.insert_text(str(n))
-            data2.select_all(); data2.delete_selection()    
-            data2.insert_text(str(phi))
-            data3.select_all(); data3.delete_selection()    
-            data3.insert_text(str(pub_key))
-        elif cifrador== "Rabin":
-            n = pk.generateRabinData(int(primo1.text),int(primo2.text))
-            data1.select_all(); data1.delete_selection()    
-            data1.insert_text(str(n))
-        elif cifrador== "ElGamal":
-            alpha, beta = pk.generateElgamalData(int(primo1.text))
-            data1.select_all(); data1.delete_selection()    
-            data1.insert_text(str(alpha))
-            data2.select_all(); data2.delete_selection()    
-            data2.insert_text(str(beta))
-        elif cifrador== "Menezes Vanestone":
-            a,b,p,gx, gy, ca, cb = pk.generateMvData(int(primo1.text))
-            data1.select_all(); data1.delete_selection()    
-            data1.insert_text(str(a))
-            data2.select_all(); data2.delete_selection()    
-            data2.insert_text(str(b))
-            data4.select_all(); data4.delete_selection()    
-            data4.insert_text(str(gx))
-            data5.select_all(); data5.delete_selection()    
-            data5.insert_text(str(gy))
-        else:
+        try:
+            if cifrador== "RSA":
+                n, phi, pub_key = pk.generateRsaData(int(primo1.text), int(primo2.text))
+                data1.select_all(); data1.delete_selection()    
+                data1.insert_text(str(n))
+                data2.select_all(); data2.delete_selection()    
+                data2.insert_text(str(phi))
+                data3.select_all(); data3.delete_selection()    
+                data3.insert_text(str(pub_key))
+            elif cifrador== "Rabin":
+                n = pk.generateRabinData(int(primo1.text),int(primo2.text))
+                data1.select_all(); data1.delete_selection()    
+                data1.insert_text(str(n))
+            elif cifrador== "ElGamal":
+                alpha, beta = pk.generateElgamalData(int(primo1.text))
+                data1.select_all(); data1.delete_selection()    
+                data1.insert_text(str(alpha))
+                data2.select_all(); data2.delete_selection()    
+                data2.insert_text(str(beta))
+            elif cifrador== "Menezes Vanestone":
+                a,b,p,gx, gy, ca, cb = pk.generateMvData(int(primo1.text))
+                data1.select_all(); data1.delete_selection()    
+                data1.insert_text(str(a))
+                data2.select_all(); data2.delete_selection()    
+                data2.insert_text(str(b))
+                data4.select_all(); data4.delete_selection()    
+                data4.insert_text(str(gx))
+                data5.select_all(); data5.delete_selection()    
+                data5.insert_text(str(gy))
+            else:
+                pass
+        except:
             pass
     def cifrar(self, cifrador, primo1, primo2, data1, data2, data3, data4, data5, Texto, Resultado):
-        if cifrador== "RSA":
-            text=pk.cifrarRSA(Texto.text, int(data1.text), int(data3.text))
-            text=list_to_str(text)
-            Resultado.select_all(); Resultado.delete_selection()   
-            Resultado.insert_text(text)
-        elif cifrador== "Rabin":
-            text=pk.cifrarRabin(Texto.text, int(data1.text))
-            text=list_to_str(text)
-            Resultado.select_all(); Resultado.delete_selection()   
-            Resultado.insert_text(text)
-        elif cifrador== "ElGamal":
-            text=pk.cifrarElgamal(Texto.text, int(primo1.text), int(data2.text), int(data1.text))
-            text=list_to_str(text)
-            Resultado.select_all(); Resultado.delete_selection()   
-            Resultado.insert_text(text)
-        elif cifrador== "Menezes Vanestone":
-            text=pk.cifrarMenezesVanestone(Texto.text)
-            Resultado.select_all(); Resultado.delete_selection()   
-            Resultado.insert_text(str(pk.cifrarMenezesVanestone(Texto.text)))
+        try:
+            if cifrador== "RSA":
+                text=pk.cifrarRSA(Texto.text, int(data1.text), int(data3.text))
+                text=list_to_str(text)
+                Resultado.select_all(); Resultado.delete_selection()   
+                Resultado.insert_text(text)
+            elif cifrador== "Rabin":
+                text=pk.cifrarRabin(Texto.text, int(data1.text))
+                text=list_to_str(text)
+                Resultado.select_all(); Resultado.delete_selection()   
+                Resultado.insert_text(text)
+            elif cifrador== "ElGamal":
+                text=pk.cifrarElgamal(Texto.text, int(primo1.text), int(data2.text), int(data1.text))
+                text=list_to_str(text)
+                Resultado.select_all(); Resultado.delete_selection()   
+                Resultado.insert_text(text)
+            elif cifrador== "Menezes Vanestone":
+                text=pk.cifrarMenezesVanestone(Texto.text)
+                Resultado.select_all(); Resultado.delete_selection()   
+                Resultado.insert_text(str(pk.cifrarMenezesVanestone(Texto.text)))
+        except:
+            pass
 
     def descifrar(self, cifrador, primo1, primo2, data1, data2, data3, data4, data5, Texto, Resultado):
-        if cifrador== "RSA":
-            text=str_to_list(Texto.text)
-            Resultado.select_all(); Resultado.delete_selection()
-            Resultado.insert_text(pk.descifrarRSA(text, int(data1.text)))
-        elif cifrador=="Rabin":
-            text=str_to_list(Texto.text)
-            text=pk.descifrarRabin(text)
-            if int(primo1.text)>99999:
-                posibleText = ""
-                for i in text:
-                    for j in i:
-                        if len(j)==3:
-                            posibleText=posibleText+j
-                text=posibleText
-            Resultado.select_all(); Resultado.delete_selection()
-            Resultado.insert_text(str(text))
-        elif cifrador=="ElGamal":
-            text=str_to_list(Texto.text)
-            Resultado.select_all(); Resultado.delete_selection()
-            Resultado.insert_text(str(pk.descifrarElgamal(text)))
-        elif cifrador=="Menezes Vanestone":
-            text=list(Texto.text)
-            text[0]='['
-            text[-1]=']'
-            text="".join(text).replace(" ","").replace("},{","],[").replace("{","(").replace("}",")")
-            text="["+text+"]"
-            text = list(text)
-            text[1]=""
-            text[-2]=""
-            text="".join(text)
-            print(text)
-            text=eval(text)
-            Resultado.select_all(); Resultado.delete_selection()
-            Resultado.insert_text(str(pk.descifrarMenezesVanestone(text)))
+        try:
+            if cifrador== "RSA":
+                text=str_to_list(Texto.text)
+                Resultado.select_all(); Resultado.delete_selection()
+                Resultado.insert_text(pk.descifrarRSA(text, int(data1.text)))
+            elif cifrador=="Rabin":
+                text=str_to_list(Texto.text)
+                text=pk.descifrarRabin(text)
+                if int(primo1.text)>99999:
+                    posibleText = ""
+                    for i in text:
+                        for j in i:
+                            if len(j)==3:
+                                posibleText=posibleText+j
+                    text=posibleText
+                Resultado.select_all(); Resultado.delete_selection()
+                Resultado.insert_text(str(text))
+            elif cifrador=="ElGamal":
+                text=str_to_list(Texto.text)
+                Resultado.select_all(); Resultado.delete_selection()
+                Resultado.insert_text(str(pk.descifrarElgamal(text)))
+            elif cifrador=="Menezes Vanestone":
+                text=list(Texto.text)
+                text[0]='['
+                text[-1]=']'
+                text="".join(text).replace(" ","").replace("},{","],[").replace("{","(").replace("}",")")
+                text="["+text+"]"
+                text = list(text)
+                text[1]=""
+                text[-2]=""
+                text="".join(text)
+                print(text)
+                text=eval(text)
+                Resultado.select_all(); Resultado.delete_selection()
+                Resultado.insert_text(str(pk.descifrarMenezesVanestone(text)))
+        except:
+            pass
 
 class FirmaDigital_Screen(Screen):
     def changeStyle(self,text):
@@ -174,10 +192,13 @@ class FirmaDigital_Screen(Screen):
             return "List", 'icon'
         else: return "Icon", 'list'
     def aplicarRuta(self, ruta, files):
-        files.path=ruta.text
-        if files.path!=ruta.text:
-            ruta.text=files.path
+        try:
             files.path=ruta.text
+            if files.path!=ruta.text:
+                ruta.text=files.path
+                files.path=ruta.text
+        except: pass
+
     def firmar(self, file, cifrador, firma):
         bool=False
         firma.text = ""
@@ -243,9 +264,18 @@ class Imagenes_Screen(Screen):
         if text== "Icon":
             return "List", 'icon'
         else: return "Icon", 'list'
+    def rutas(self, image, files):
+        try:
+            image.source = str(files.selection[0])
+            image.opacity= 1
+        except:
+            pass
         
     def cifrar(self, image, A, B):
-        im.encriptarImage(image.source, image, A, B)
+        try:
+            im.encriptarImage(image.source, image, A, B)
+        except: 
+            pass
 
 class Blockchain_Screen(Screen):
     def transaccion(self, remitente, destinatario, cantidad, texto):
@@ -284,6 +314,12 @@ class Blockchain_Screen(Screen):
             Popup_Screen().setText("El bloque ha sido creado. Se han verificado todas las transacciones.")
             Popup_Screen().open()
             trans.text = ""
+            indice = Bc.indice_ultima_transaccion
+            transac = Bc.Transacciones
+            for j in transac[indice: len(transac)]:
+                resultado = Bc.mostrarTransaccion(j)
+                for i in resultado:
+                    trans.text= trans.text+i
         else:
             Popup_Screen().setText("Parece que no hay suficientes transacciones para generar un bloque.")
             Popup_Screen().open()
@@ -291,9 +327,12 @@ class Blockchain_Screen(Screen):
 
     def Full_Blockchain(self):
         text = ""
-        for i in Bc.Full_blockchain(Bc.MoonCoins):
-            text = text+i
-            Full_Blockchain_Screen.setText(text)
+        try:
+            for i in Bc.Full_blockchain(Bc.MoonCoins):
+                text = text+i
+                Full_Blockchain_Screen.setText(text)
+        except:
+            pass
 
 class Full_Blockchain_Screen(Screen):
     space = TextInput()
